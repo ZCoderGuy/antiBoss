c1efe2f5235c456d356f197ecc5f69bc6e5ef4f8
Max Zitnick <maxzitnick@gmail.com>
2017-08-27 20:30:19 -0500

    wrote the code

--- antiboss.py
+++ antiboss.py
@@ -1,1 +1,13 @@
+import pyautogui
+
+#Opening the task manager
+pyautogui.keyDown('esc', 'shift')
+pyautogui.keyUp('esc', 'shift')
+
+#Finding the iBoss process and closing it
+output = pyautogui.locateOnScreen('iboss.png')
+location = pyautogui.center(output)
+pyautogui.click(location)
+pyautogui.typewrite('enter')
+
 
