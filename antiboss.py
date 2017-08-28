f8dea25a154ebb2286b02e0898b8a1ff5204e6d5
Max Zitnick <maxzitnick@gmail.com>
2017-08-28 12:14:27 -0500

    finished the file

--- antiboss.py
+++ antiboss.py
@@ -1,5 +1,9 @@
 import pyautogui
 
+#Query the user on the destination site
+askUser = input("Where would you like to go?")
+destination = list(askUser)
+
 #Opening the task manager
 pyautogui.keyDown('esc', 'shift')
 pyautogui.keyUp('esc', 'shift')
@@ -10,4 +14,9 @@ location = pyautogui.center(output)
 pyautogui.click(location)
 pyautogui.typewrite('enter')
 
+#Select the address bar and go to the requested website
+pyautogui.keyDown('control', 'l')
+pyautogui.keyUp('control', 'l')
+pyautogui.typewrite(destination)
+pyautogui.typewrite('enter')
 
